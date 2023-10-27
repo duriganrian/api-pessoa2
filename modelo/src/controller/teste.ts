61326e7032a6f338734dd8476c3b0c5694deb4c7
duriganrian <126262350+duriganrian@users.noreply.github.com>
2023-10-27 09:04:10 -0300

    Update teste.ts

--- modelo/src/controller/teste.ts
+++ modelo/src/controller/teste.ts
@@ -1,28 +1,6 @@
 
 import { Pessoa } from "../model/pessoa";
 
-let pessoa = new Pessoa (`Rian`, `5555555555`, new Date(2000, 9, 15), `000000000000`, `Rua Número 1`, 170, 70);
+let pessoa = new Pessoa('Maria zeferina', '33333333333333333', new Date (1972, 8, 29), '1111111', 'Rua das corridas', 165, 55);
 
-pessoa.mostraPessoa();
-
-pessoa.falar();
-pessoa.falarFrase("VAI CURINTIA!!!! BANDO DE OREIUDOO");
-pessoa.andar();
-pessoa.andarRapido(15);
-pessoa.comer();
-pessoa.comerPrato("parmegiana");
-
-let pessoa2 = new Pessoa (`Francisley`, `22343557890`, new Date (1899, 4, 13), `99193456000`, `Rua numero 123`, 220, 20);
-pessoa.mostraPessoa();
-pessoa.falar();
-pessoa.falarFrase("Ooo meu fii");
-pessoa.comer();
-pessoa.comerPrato("Açai");
-
-let pessoa3 = new Pessoa (`jubiscleudo`, `1234587630`, new Date(1630, 3, 22), `12345633`, `Rua numero 33`, 233, 56);
-
-pessoa.mostraPessoa();
-pessoa.falar();
-pessoa.falarFrase("Aqui e corinthians");
-pessoa.comer();
-pessoa.comerPrato("Macarrao");
+pessoa.cadastroPessoa(pessoa);
