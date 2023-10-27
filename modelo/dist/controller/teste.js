bfd030dde84acc06f93fa330c84faf95612a1640
duriganrian <126262350+duriganrian@users.noreply.github.com>
2023-10-27 09:03:54 -0300

    Update teste.js

--- modelo/dist/controller/teste.js
+++ modelo/dist/controller/teste.js
@@ -1,16 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const pessoa_1 = require("../model/pessoa");
-let pessoa = new pessoa_1.Pessoa(`Rian`, `5555555555`, new Date(2000, 9, 15), `000000000000`, `Rua Número 1`, 170, 70);
-pessoa.mostraPessoa();
-pessoa.falar();
-pessoa.falarFrase("VAI CURINTIA!!!! BANDO DE OREIUDOO");
-pessoa.andar();
-pessoa.andarRapido(15);
-pessoa.comer();
-pessoa.comerPrato("parmegiana");
-let pessoa2 = new pessoa_1.Pessoa(`Francisley`, `22343557890`, new Date(1899, 4, 13), `99193456000`, `Rua numero 123`, 220, 20);
-pessoa.comer();
-pessoa.comerPrato("");
-pessoa;
+let pessoa = new pessoa_1.Pessoa('Maria zeferina', '33333333333333333', new Date(1972, 8, 29), '1111111', 'Rua das corridas', 165, 55);
+pessoa.cadastroPessoa(pessoa);
 //# sourceMappingURL=teste.js.map
